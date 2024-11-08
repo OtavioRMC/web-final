eac9d2d6193c6da87a2198ee2d44c5494cbcbc71
OtavioRMC <otavioribeiromoreiradacosta@gmail.com>
2024-11-08 15:30:44 -0300

    feat: adicionado lógica de cadastro de usuário no backend.

---
+++ cadastro.php
@@ -0,0 +1,43 @@
+<?php
+
+ include 'db.php';
+
+
+ // Recebe as informaçãoes via formulário
+ $nome = $_POST["nome"];
+ $email = $_POST["email"];
+ $data_nascimento = $_POST["data_nasc"];
+ $telefone = $_POST["telefone"];
+ $senha = $_POST["senha"];
+ $cep = $_POST["cep"];
+ $rua = $_POST["rua"];
+ $num = $_POST["numero"];
+ $bairro = $_POST["bairro"];
+ $compl = $_POST["complemento"];
+ $cidade = $_POST["cidade"];
+ $estado = $_POST["estado"];
+
+echo"antes da instrução";
+
+ $sql = "INSERT INTO tb_usuario (nome, email, data_nascimento, telefone, senha, cep, rua, numero, bairro, complemento, cidade, estado) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+ $cmd = $conn->prepare($sql);
+
+ 
+if ($cmd) {
+  // Associar parâmetros a instrução sql.
+  $cmd->bind_param("ssssssssssss", $nome, $email, $data_nascimento, $telefone, $senha, $cep, $rua, $num, $bairro, $compl, $cidade, $estado);
+  
+  // Executa a query
+  if ($cmd->execute()) {
+      echo "Usuário cadastrado com sucesso!";
+  } else {
+      echo "Erro ao cadastrar usuário." . $cmd->error;
+  }
+} else {
+  echo "Falha na preparação da consulta." .$conn->error;
+}
+
+// Fecha a comunicação com o banco.
+$conn->close();
+
+?>
